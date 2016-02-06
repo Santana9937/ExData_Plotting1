70c88ef3ccf8872de22acc4de7e00d38ea8111f0
Santana9937 <Santana9937@gmail.com>
2016-02-05 21:05:06 -0800

    Added finished plot2 and plot3 scrips and images

--- plot3.R
+++ plot3.R
@@ -23,25 +23,36 @@ power_data$Time <- as.character(power_data$Time)
 # timestamp. This is down by pasting the strings in the 
 # Date and Time columns together in the format shown below.
 power_data$timestamp <- as.POSIXct(paste(power_data$Date,power_data$Time, sep=" "),
-                                 format="%d/%m/%Y %H:%M:%S")
+                                   format="%d/%m/%Y %H:%M:%S")
 
 # Defining the start and end dates for subsetting the date
 start_date <- as.POSIXct("01/02/2007 00:00:00", 
-                       format="%d/%m/%Y %H:%M:%S")
+                         format="%d/%m/%Y %H:%M:%S")
 end_date <- as.POSIXct("02/02/2007 23:59:59", 
-                     format="%d/%m/%Y %H:%M:%S")
+                       format="%d/%m/%Y %H:%M:%S")
 
-# Subset of global active power between start date and end date
-gac_subs <- subset(power_data$Global_active_power, 
+# Subset of Sub_metering_1, Sub_metering_2, and
+# Sub_metering_3 between start date and end date
+subs_met_1 <- subset(power_data$Sub_metering_1, 
                    power_data$timestamp>=start_date & power_data$timestamp<=end_date)
+subs_met_2 <- subset(power_data$Sub_metering_2, 
+                     power_data$timestamp>=start_date & power_data$timestamp<=end_date)
+subs_met_3 <- subset(power_data$Sub_metering_3, 
+                     power_data$timestamp>=start_date & power_data$timestamp<=end_date)
+
 
 # Subset of timestamp between start date and end date
 timestamp_subs <- subset(power_data$timestamp, 
-                        power_data$timestamp>=start_date & power_data$timestamp<=end_date)
+                         power_data$timestamp>=start_date & power_data$timestamp<=end_date)
 
 # Plotting the subset of the Global_active_power we are interested in
-png(filename="plot2.png", width=480, height=480)
-plot(timestamp_subs, gac_subs, col = "black", type= "l", 
-     xlab="", ylab="Global Active Power (kilowatts)", main=NULL)
+png(filename="plot3.png", width=480, height=480)
+plot(timestamp_subs, subs_met_1, col = "black", type= "l", 
+     xlab="", ylab="Energy sub metering", main=NULL)
+lines(timestamp_subs, subs_met_2,col="red")
+lines(timestamp_subs, subs_met_3,col="blue")
+legend("topright", 
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty=c(1,1,1), col=c("black","red","blue") )
 dev.off()
 
